87d841d30ffd6c1eb5ce01a0bed70ebb3e4ddae5
adaeze <adaeze.ikegbule@univaciti.com>
2026-09-06 18:41:06 +0100

    Add menu photography

---
+++ server/images.sql
@@ -0,0 +1,83 @@
+-- Chowly: link menu items to their images
+
+UPDATE menu_items SET image_url = '/menu/peppersoup.jpg'             WHERE name = 'Pepper Soup';
+UPDATE menu_items SET image_url = '/menu/suyaskewers.jpg'            WHERE name = 'Suya Skewers';
+UPDATE menu_items SET image_url = '/menu/chickenwings.jpg'           WHERE name = 'Chicken Wings';
+UPDATE menu_items SET image_url = '/menu/puffpuff.jpg'               WHERE name = 'Puff Puff';
+UPDATE menu_items SET image_url = '/menu/springrolls.jpg'            WHERE name = 'Spring Rolls';
+UPDATE menu_items SET image_url = '/menu/samosa.jpg'                 WHERE name = 'Samosa';
+UPDATE menu_items SET image_url = '/menu/meatpie.jpg'                WHERE name = 'Meat Pie';
+UPDATE menu_items SET image_url = '/menu/sausagerolls.jpg'           WHERE name = 'Sausage Rolls';
+UPDATE menu_items SET image_url = '/menu/asun.jpg'                   WHERE name = 'Asun';
+UPDATE menu_items SET image_url = '/menu/pepperedsnail.jpg'          WHERE name = 'Peppered Snail';
+
+UPDATE menu_items SET image_url = '/menu/jollofriceandchicken.jpg'   WHERE name = 'Jollof Rice & Chicken';
+UPDATE menu_items SET image_url = '/menu/friendriceandturkey.jpg'    WHERE name = 'Fried Rice & Turkey';
+UPDATE menu_items SET image_url = '/menu/ofadariceandayamase.jpg'    WHERE name = 'Ofada Rice & Ayamase';
+UPDATE menu_items SET image_url = '/menu/egusiandpoundedyam.jpg'     WHERE name = 'Egusi & Pounded Yam';
+UPDATE menu_items SET image_url = '/menu/eforiro.jpg'                WHERE name = 'Efo Riro & Semo';
+UPDATE menu_items SET image_url = '/menu/okro.jpg'                   WHERE name = 'Okra Soup & Amala';
+UPDATE menu_items SET image_url = '/menu/afangsoup.jpg'              WHERE name = 'Afang Soup & Eba';
+UPDATE menu_items SET image_url = '/menu/bangasoupandstarch.jpg'     WHERE name = 'Banga Soup & Starch';
+UPDATE menu_items SET image_url = '/menu/grilledtilapia.jpg'         WHERE name = 'Grilled Tilapia';
+UPDATE menu_items SET image_url = '/menu/nkwobi.jpg'                 WHERE name = 'Nkwobi';
+
+UPDATE menu_items SET image_url = '/menu/adanakebab.jpg'             WHERE name = 'Adana Kebab';
+UPDATE menu_items SET image_url = '/menu/chickenshawarmabowl.jpg'    WHERE name = 'Chicken Shawarma Bowl';
+UPDATE menu_items SET image_url = '/menu/turkishpasta.jpg'           WHERE name = 'Turkish Pasta';
+UPDATE menu_items SET image_url = '/menu/chickenchickpeacurry.jpg'   WHERE name = 'Chicken Chickpea Curry';
+UPDATE menu_items SET image_url = '/menu/lambburger.jpg'             WHERE name = 'Lamb Burger';
+UPDATE menu_items SET image_url = '/menu/shreddedchickennachos.jpg'  WHERE name = 'Shredded Chicken Nachos';
+UPDATE menu_items SET image_url = '/menu/salmoncoconutsoup.jpg'      WHERE name = 'Salmon Coconut Soup';
+UPDATE menu_items SET image_url = '/menu/beefstirfryandnoodles.jpg'  WHERE name = 'Beef Stir-Fry & Noodles';
+UPDATE menu_items SET image_url = '/menu/margheritapizza.jpg'        WHERE name = 'Margherita Pizza';
+UPDATE menu_items SET image_url = '/menu/fishandchips.jpg'           WHERE name = 'Fish & Chips';
+
+UPDATE menu_items SET image_url = '/menu/friedplantain.jpg'          WHERE name = 'Fried Plantain';
+UPDATE menu_items SET image_url = '/menu/jollofrice.jpg'             WHERE name = 'Jollof Rice';
+UPDATE menu_items SET image_url = '/menu/skinonfries.jpg'            WHERE name = 'Skin-On Fries';
+UPDATE menu_items SET image_url = '/menu/coleslaw.jpg'               WHERE name = 'Coleslaw';
+UPDATE menu_items SET image_url = '/menu/moimoi.jpg'                 WHERE name = 'Moi Moi';
+UPDATE menu_items SET image_url = '/menu/shephardsalad.jpg'          WHERE name = 'Shepherd Salad';
+UPDATE menu_items SET image_url = '/menu/garlicbread.jpg'            WHERE name = 'Garlic Bread';
+
+UPDATE menu_items SET image_url = '/menu/chocolatefondant.jpg'       WHERE name = 'Chocolate Fondant';
+UPDATE menu_items SET image_url = '/menu/puffpuffandicecream.jpg'    WHERE name = 'Puff Puff & Ice Cream';
+UPDATE menu_items SET image_url = '/menu/chinchinsundae.jpg'         WHERE name = 'Chin Chin Sundae';
+UPDATE menu_items SET image_url = '/menu/coconutcandy.jpg'           WHERE name = 'Coconut Candy';
+UPDATE menu_items SET image_url = '/menu/cheesecake.jpg'             WHERE name = 'Cheesecake';
+
+UPDATE menu_items SET image_url = '/menu/stillwater.jpg'             WHERE name = 'Still Water';
+UPDATE menu_items SET image_url = '/menu/sparklingwater.jpg'         WHERE name = 'Sparkling Water';
+UPDATE menu_items SET image_url = '/menu/softdrink.jpg'              WHERE name = 'Soft Drink';
+UPDATE menu_items SET image_url = '/menu/freshorangejuice.jpg'       WHERE name = 'Fresh Orange Juice';
+UPDATE menu_items SET image_url = '/menu/zobo.jpg'                   WHERE name = 'Zobo';
+UPDATE menu_items SET image_url = '/menu/mangosmoothie.jpg'          WHERE name = 'Mango Smoothie';
+UPDATE menu_items SET image_url = '/menu/chocolatemilkshake.jpg'     WHERE name = 'Chocolate Milkshake';
+UPDATE menu_items SET image_url = '/menu/chapman.jpg'                WHERE name = 'Chapman';
+UPDATE menu_items SET image_url = '/menu/virginmojito.jpg'           WHERE name = 'Virgin Mojito';
+UPDATE menu_items SET image_url = '/menu/virginpinacolada.jpg'       WHERE name LIKE 'Virgin Pi%a Colada';
+UPDATE menu_items SET image_url = '/menu/fruitpunch.jpg'             WHERE name = 'Fruit Punch';
+UPDATE menu_items SET image_url = '/menu/berryfizz.jpg'              WHERE name = 'Berry Fizz';
+
+UPDATE menu_items SET image_url = '/menu/strawberrydaiquiri.jpg'     WHERE name = 'Strawberry Daiquiri';
+UPDATE menu_items SET image_url = '/menu/pornstarmartini.jpg'        WHERE name = 'Pornstar Martini';
+UPDATE menu_items SET image_url = '/menu/mojito.jpg'                 WHERE name = 'Mojito';
+UPDATE menu_items SET image_url = '/menu/margarita.jpg'              WHERE name = 'Margarita';
+UPDATE menu_items SET image_url = '/menu/pinacolada.jpg'             WHERE name LIKE 'Pi%a Colada';
+UPDATE menu_items SET image_url = '/menu/cosmopolitan.jpg'           WHERE name = 'Cosmopolitan';
+UPDATE menu_items SET image_url = '/menu/longislandicedtea.jpg'      WHERE name = 'Long Island Iced Tea';
+UPDATE menu_items SET image_url = '/menu/espressomartini.jpg'        WHERE name = 'Espresso Martini';
+UPDATE menu_items SET image_url = '/menu/tequilasunrise.jpg'         WHERE name = 'Tequila Sunrise';
+UPDATE menu_items SET image_url = '/menu/starlager.jpg'              WHERE name = 'Star Lager';
+UPDATE menu_items SET image_url = '/menu/gulder.jpg'                 WHERE name = 'Gulder';
+UPDATE menu_items SET image_url = '/menu/guinnessstout.jpg'          WHERE name = 'Guinness Stout';
+UPDATE menu_items SET image_url = '/menu/heineken.jpg'               WHERE name = 'Heineken';
+UPDATE menu_items SET image_url = '/menu/redwine.jpg'                WHERE name = 'Red Wine';
+UPDATE menu_items SET image_url = '/menu/whitewine.jpg'              WHERE name = 'White Wine';
+UPDATE menu_items SET image_url = '/menu/presecco.jpg'               WHERE name = 'Prosecco';
+UPDATE menu_items SET image_url = '/menu/jameson.jpg'                WHERE name = 'Jameson';
+UPDATE menu_items SET image_url = '/menu/jackdaniels.jpg'            WHERE name = 'Jack Daniel''s';
+UPDATE menu_items SET image_url = '/menu/greygoose.jpg'              WHERE name = 'Grey Goose';
+UPDATE menu_items SET image_url = '/menu/josecuervo.jpg'             WHERE name = 'Jose Cuervo';
+UPDATE menu_items SET image_url = '/menu/bombaysapphire.jpg'         WHERE name = 'Bombay Sapphire';
